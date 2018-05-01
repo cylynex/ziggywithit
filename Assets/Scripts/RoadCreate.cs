df99a39817d7124ff14a0506f17e40b23c5e4cda
Santo Commarato <cylynex@gmail.com>
2018-05-01 10:58:08 -0600

    Camera locked to front, destroy excess blocks behind

--- Assets/Scripts/RoadCreate.cs
+++ Assets/Scripts/RoadCreate.cs
@@ -5,16 +5,24 @@ using UnityEngine;
 public class RoadCreate : MonoBehaviour {
 
     public GameObject roadCubePrefab;
+    public Camera camera;
+
+    private Vector3 offset;
+
     Vector3 startPosition = new Vector3(0, 0, 0);
     Vector3 currentPosition = new Vector3(0, 0, 0);
+    Vector3 lastPosition = new Vector3(0, 0, 0); 
 
     void Start() {
-        InvokeRepeating("AddRoad", 1f, 1f);
+        InvokeRepeating("AddRoad", 1f, 0.5f);
         currentPosition = startPosition;
+        offset = camera.transform.position - startPosition;
     }
 
 
     void AddRoad() {
+
+        // Create the new block
         GameObject currentRoad = (GameObject)Instantiate(roadCubePrefab, currentPosition, Quaternion.identity);
 
         // Decide if there is a crystal here or not.  If so, set it active (since technically its always "there"
@@ -25,9 +33,11 @@ public class RoadCreate : MonoBehaviour {
             crystal.gameObject.SetActive(true);
         }
 
+        // Preserve the prior position for camera movement
+        lastPosition = currentPosition;
+
         // Determine x or z direction with a random roll to generate next block
         int direction = Random.Range(1, 100);
-        Debug.Log("moving " + direction);
 
         // Setup the next position
         if (direction <= 50) {
@@ -39,6 +49,18 @@ public class RoadCreate : MonoBehaviour {
             float moveZ = currentPosition.z + 1;
             currentPosition = new Vector3(currentPosition.x, currentPosition.y, moveZ);
         }
+
+        // Move the camera
+        GameObject thisCam = camera.gameObject;
+        thisCam.transform.position = currentRoad.transform.position + offset;
+
+        // Setup the block to be destroyed behind the player
+        Destroy(currentRoad, 10f);
+
+    }
+
+    void LateUpdate() {
+        
 
     }
 
